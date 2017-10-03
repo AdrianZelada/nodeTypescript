f22984356e28618cd8a9d7d8f57d4aaf446e9dde
adrianZelada <zelada_torrez@hotmail.com>
2017-10-03 00:49:06 -0400

    users

--- src/controllers/abstractControl.ts
+++ src/controllers/abstractControl.ts
@@ -2,26 +2,80 @@
  * Created by iZel on 10/2/17.
  */
 import { Request, Response, NextFunction , Router} from "express";
+import { Model} from "mongoose";
 
 export class abstractControl{
 
     model:any;
-    router:Router;
-    constructor(public schema:any,public route:Router){
+    // router:Router;
+    defaultRoute:any={
+        '/'         : 'list',
+        'POST /'    : 'create',
+        'PUT /'     : 'update',
+        'DELETE /:id'  : 'remove'
+    };
+
+
+    constructor(public schema:any, public router:Router,public extendRoute:any={}){
         this.model=schema;
-        this.router=route;
-        this.router.get('/',this.list.bind(this));
+        // this.router=<Router>route;
+        this.defaultRoute=this.model ? this.defaultRoute:{};
+        this.defaultRoute=Object.assign({},this.defaultRoute,extendRoute);
+        this.registerRoutes();
+    }
+
+    registerRoutes() {
+
+        let _this:any=this;
+
+        Object.keys(_this.defaultRoute).forEach((path) => {
+            let method = _this.defaultRoute[path];
+            let verb = path.split(' ').length > 1 ? path.split(' ')[0] : 'get';
+            verb = verb.toLowerCase();
+            _this.router[verb](path, _this[method].bind(_this));
+        });
     }
 
     list(req:Request,res:Response){
-        this.model.find((e:any,data:any)=>{
-            if(e){
-                res.json({
-                    error:true
-                })
-            }
-            res.json(data);
+        this.model.find(function (err:any,resp:any) {
+            if(err) this.handleError(res)(err);
+            return res.json(resp);
+        });
+    }
+
+
+
+    create(req:Request,res:Response){
+        let body= req.body;
+        let model=new this.model(body);
+        model.save(function (err:any,data:any) {
+            if(err) return this.handleError(res)(err);
+            return res.json(data)
         })
+    }
+
+    update(req:Request,res:Response){
+        let query = {
+            _id:req.body._id
+        };
+        this.model.findOneAndUpdate(query,req.body, {new:true},function (err:any, doc:any) {
+            if (err) this.handleError(res)(err);
+            res.json(doc);
+        })
+    }
+
+    remove(req:Request,res:Response){
+        this.model.remove({_id:req.params.id},function (err:any,data:any) {
+            if(err) return this.handleError(res)(err);
+            return res.json(data)
+        })
+    }
+
+
+    handleError(res:Response) {
+        return function(error:any) {
+            return res.status(500).json(error);
+        }
     }
 
 }
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,13 +2,12 @@
  * Created by iZel on 10/2/17.
  */
 import {default as User} from '../models/User';
-import { Request, Response, NextFunction,Router } from "express";
+import { Request, Response, NextFunction,IRoute } from "express";
 import { abstractControl} from './abstractControl';
 
 export class userController extends abstractControl{
 
-
-    constructor(router:Router){
+    constructor(router:any){
         super(User,router);
     }
 }
